ea8816b72d871a31acb5810cff21c5e94aec5976
michalwojcieszek <mwoj772@gmail.com>
2024-02-04 16:38:10 +0100

    seeder created

---
+++ backend/seeder.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+import users from "./data/users";
+import products from "./data/products.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel";
+import Order from "./models/orderModel.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+
+    //first from users.js is an admin
+    const adminUser = createdUsers[0]._id;
+
+    //adding admin user's id to each product
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    //colors package
+    console.log("Data imported!".green.inverse);
+    //inserting all the products into the database
+    await Product.insertMany(sampleProducts);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log("data destroyed!".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
