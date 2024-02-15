7fa6c5a831fe275309a86b7c03cf90ed79d75eab
michalwojcieszek <mwoj772@gmail.com>
2024-02-15 17:03:58 +0100

    redux installed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,16 @@
 import express from "express";
-import products from "../data/products.js";
-import asyncHandler from "../middleware/asyncHandler.js";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 import Product from "../models/productModel.js";
-
+import asyncHandler from "../middleware/asyncHandler.js";
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    //mongoose method
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+// previously router.get("/", getProducts);
+router.route("/").get(getProducts);
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      return res.json(product);
-    }
-    res.status(404).json({ message: "Product not found" });
-  })
-);
+// previously router.get("/:id", getProductById);
+router.route("/:id").get(getProductById);
 
 export default router;
